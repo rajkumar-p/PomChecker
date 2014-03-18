bf59b840a69249c446a56a1353ecb9f245ff392e
Rajkumar P <raj@diskodev.com>
2014-03-18 19:39:12 +0530

    printDirBanner now takes file handle as a parameter.
    Signature of printDirBanner is now changed from
            printDirBanner(current_dir)
    
            to
    
            printDirBanner(file_handle, current_dir)

--- pomchecker.py
+++ pomchecker.py
@@ -103,7 +103,7 @@ def printHeader(title):
     print title
     print "-" * len(title)
 
-def printDirBanner(current_dir):
+def printDirBanner(file_handle, current_dir):
     """
     Helper to print a banner around the current directory
     input: current_dir
@@ -212,7 +212,7 @@ while not directories.empty():
     pom_file = current_dir + os.path.sep + "pom.xml"
     pom_template_file = current_dir + os.path.sep + "pom.template.xml"
 
-    printDirBanner(current_dir)
+    printDirBanner(logger, current_dir)
 
     pom_present_flag = True
     pom_template_present_flag = True
